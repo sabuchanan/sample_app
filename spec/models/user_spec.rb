eb92a02c9b94cd5b26a54518765c4d7e76d478ba
Steve Buchanan <sabuchanan@yahoo.com>
2013-05-01 18:26:30 -0400

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,9 @@
 #  id         :integer          not null, primary key
 #  name       :string(255)
 #  email      :string(255)
+#  password_digest      :string
+#  remember_token      :string
+#  admin      :boolean          false
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
@@ -26,10 +29,21 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
+  it { should_not be_admin }
   
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
   describe "when name is not present" do
     before { @user.name = " " }
 	it { should_not be_valid }
